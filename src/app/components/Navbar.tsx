0a15a5ed252ab1cd231ac092222073dd82a4272e
DecantsPerfumes <decantsperfumes@outlook.com>
2023-10-24 12:20:59 -0300

    Navbar con logo

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -2,12 +2,21 @@ import React from 'react';
 import { authOptions } from '../api/auth/[...nextauth]/route';
 import { getServerSession } from 'next-auth/next';
 import Link from 'next/link';
+import paolamenaImage from '../../../public/Images/paolamena.png';
 
 const Navbar = async () => {
     const session = await getServerSession(authOptions);
 
     return (
         <div className='w-full px-4 py-8 bg-gray-300 flex flex-row items-center gap-4'>
+            <div className="flex justify-between items-center">
+                {/* Logo */}
+                <div>
+                <a href="/">
+                        <img src={paolamenaImage.src} alt="Paola Mena" width={60} height={30} />
+                    </a>
+                </div>
+            </div>
             <Link href='/'>Home</Link>
             <Link href='/protected/dashboard'>Dashboard</Link>
 
